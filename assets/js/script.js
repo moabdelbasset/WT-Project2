b1be9196c5d318a2d05ce7854fa04e83148be711
moabdelbasset <>
2023-04-27 19:01:54 +0000

    add JS structre

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,39 @@
-console.log("Connected!");
+function runGame() {
+
+
+}
+
+function checkAnswer() {
+
+
+}
+
+function calculateCorrectAnswer() {
+
+
+}
+
+function incrementScore() {
+
+
+}
+
+function incrementWrongAnswer() {
+
+
+}
+
+function displayAdditionQuestion() {
+
+
+}
+
+function displaySubtractQuestion() {
+
+
+}
+
+function displayMultiplyQuestion() {
+
+
+}
